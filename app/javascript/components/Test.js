d40de4513c7db2ca7a323401beac2addc14ce39b
Henry Doan <henrydoan96@gmail.com>
2021-03-08 20:13:24 -0700

    back

---
+++ app/javascript/components/Test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Test = ({ courses }) => (
+  <h1>Testing</h1>
+)
+
+export default Test
---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+roles = ['teacher', 'ta', 'student']
+5.times do
+  course = Course.create(
+    name: Faker::Beer.brand
+  )
+
+  3.times do
+    user = User.create(
+      first_name: Faker::ChuckNorris.fact,
+      last_name: Faker::Kpop.solo
+    )
+
+    Enrollment.create(
+      role: roles.sample,
+      course_id: course.id,
+      user_id: user.id
+    )
+  end
+end
+
+puts "Data Seeded"
